e53ce43c7c2db0704a1ff175068f037f6884fb58
alexjoel42 <alex.copperman@opentrons.com>
2025-03-20 17:16:32 -0400

    finally figured out move lid

--- Template_Protocols/TC_LID/new_tc_lid/tc_lid_off_deck_ml.py
+++ Template_Protocols/TC_LID/new_tc_lid/tc_lid_off_deck_ml.py
@@ -8,6 +8,8 @@ requirements = {"robotType": "Flex", "apiLevel": "2.23"}
 LID_COUNT = 5
 LID_DEFINITION = "opentrons_tough_pcr_auto_sealing_lid"
 LID_BOTTOM_DEFINITION = "opentrons_tough_pcr_auto_sealing_lid"
+DECK_RISER_NAME = "opentrons_flex_deck_riser"
+
 
 def run(protocol: ProtocolContext):
     # Tiprack to occupy space above test trashbin for collision checking
@@ -15,12 +17,6 @@ def run(protocol: ProtocolContext):
 
     tiprack_1000 = protocol.load_labware(load_name='opentrons_flex_96_tiprack_50ul', location="B2")
     pipette = protocol.load_instrument("flex_8channel_50", "right", tip_racks=[tiprack_1000])
-
-    pipette.pick_up_tip()
-    pipette.return_tip()
-
-
-
     thermocycler = protocol.load_module("thermocyclerModuleV2")
     thermocycler.open_lid()
     plate_in_cycler = thermocycler.load_labware(
@@ -28,14 +24,14 @@ def run(protocol: ProtocolContext):
     )
     trash = protocol.load_trash_bin('A3')
     #pipette.aspirate(10, plate_in_cycler['A1'])
-    morelids = protocol.load_labware(LID_BOTTOM_DEFINITION, 'D1')
-    lids = [morelids.load_labware(LID_BOTTOM_DEFINITION)]
+    lid = protocol.load_lid_stack(LID_DEFINITION, "A2", 5, adapter =DECK_RISER_NAME )
+    '''
     for i in range(LID_COUNT - 2):
         lids.append(lids[-1].load_labware(LID_DEFINITION))
     lids.reverse()  # NOTE: reversing to more easily loop through lids from top-to-bottom
     i = 0
-    for lid in lids:
-        print(i)
+''' 
+    for i in range(4):
         protocol.move_lid(
             lid,
             plate_in_cycler,
@@ -43,18 +39,10 @@ def run(protocol: ProtocolContext):
         )
         thermocycler.close_lid()
         thermocycler.open_lid()
-        if i == 0:
-            protocol.move_lid(
-                lid,
-                "D3",
-                use_gripper=True)
-        else:
-            protocol.move_lid(
-                lid,
-                lids[i-1],
-                use_gripper=True,
-            )
-        i = i + 1
+        protocol.move_lid(plate_in_cycler,"D3",use_gripper=True)
+
+  
+    
 
 
 
--- Template_Protocols/TC_LID/new_tc_lid/tc_lid_to_trash_ml.py
+++ Template_Protocols/TC_LID/new_tc_lid/tc_lid_to_trash_ml.py
@@ -1,43 +1,41 @@
-from typing import List
+from typing import List, Dict, Any, Optional
 from opentrons.protocol_api import ProtocolContext, Labware
 
-metadata = {"protocolName": "Opentrons Flex TC Lid Trash Test"}
+metadata = {"protocolName": "Opentrons Flex Deck Riser with TC Lids Test"}
 requirements = {"robotType": "Flex", "apiLevel": "2.23"}
 
-
-LID_COUNT = 5
 LID_DEFINITION = "opentrons_tough_pcr_auto_sealing_lid"
-LID_BOTTOM_DEFINITION = "opentrons_tough_pcr_auto_sealing_lid"
+DECK_RISER_NAME = "opentrons_flex_deck_riser"
+USING_THERMOCYCLER = True
 
 def run(protocol: ProtocolContext):
-    # Tiprack to occupy space above test trashbin for collision checking
-    tiprack = protocol.load_labware("opentrons_flex_96_tiprack_200ul", 'C3')
+    # SETUP
+    deck_riser_adapter = protocol.load_adapter(DECK_RISER_NAME, "A2")
+    trash = protocol.load_trash_bin("A3")
+
+    stack = protocol.load_lid_stack(LID_DEFINITION, "B2", 5)
+    
     thermocycler = protocol.load_module("thermocyclerModuleV2")
     thermocycler.open_lid()
-    plate_in_cycler = thermocycler.load_labware(
-        "opentrons_96_wellplate_200ul_pcr_full_skirt"
-    )
-    trash = protocol.load_trash_bin('A3')
-    deck_riser_adapter = protocol.load_adapter("opentrons_flex_deck_riser", 'D1')
-    lids: List[Labware] = [deck_riser_adapter.load_labware(LID_BOTTOM_DEFINITION, 'D1')]
-    for i in range(LID_COUNT - 1):
-        lids.append(lids[-1].load_labware(LID_DEFINITION))
-    lids.reverse()  # NOTE: reversing to more easily loop through lids from top-to-bottom
-
-    for lid in lids:
-        protocol.move_lid(
-            lid,
-            plate_in_cycler,
-            use_gripper=True,
-        )
-        thermocycler.close_lid()
-        thermocycler.open_lid()
-        protocol.move_lid(
-            lid,
-            trash,
-            use_gripper=True,
-        )
+    plate_in_cycler = thermocycler.load_labware("armadillo_96_wellplate_200ul_pcr_full_skirt")
 
 
+    protocol.move_lid(stack, plate_in_cycler, True)
 
+    # Move the lid to the empty riser, creating a new lid stack
+    used_stack = protocol.move_lid(plate_in_cycler, deck_riser_adapter, True)
+    protocol.move_lid(stack, plate_in_cycler, True)
+    protocol.move_lid(plate_in_cycler, used_stack, True)
+    protocol.move_lid(stack, plate_in_cycler, True)
+    protocol.move_lid(plate_in_cycler, used_stack, True)
+    protocol.move_lid(stack, plate_in_cycler, True)
+    protocol.move_lid(plate_in_cycler, used_stack, True)
+    protocol.move_lid(stack, plate_in_cycler, True)
+    protocol.move_lid(plate_in_cycler, used_stack, True)
 
+    # Trash all the lids
+    protocol.move_lid(used_stack, trash, True)
+    protocol.move_lid(used_stack, trash, True)
+    protocol.move_lid(used_stack, trash, True)
+    protocol.move_lid(used_stack, trash, True)
+    protocol.move_lid(used_stack, trash, True)
